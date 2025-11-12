6f244948cb64c18f8adc6871b25e5d7b1f475c16
a22400561Duarte Martins <iduartemt05.04@gmail.com>
2025-11-12 20:02:33 +0000

    lab8

---
+++ app/sobre/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <>
+            <h2>Desenvolvimento Web Moderno</h2>
+            <ul>
+                <li>React e Next.js revolucionara a criacao de interfaces web.            </li>
+                <li>Sao usados por empresas como Facebook, Nextflix e Airbnb.</li>
+                <li>Dominar estas tecnologias abre muitas oportunidades!</li>
+            </ul>
+        </>
+    )
+}
